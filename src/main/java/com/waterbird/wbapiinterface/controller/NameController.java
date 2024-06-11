4be76cab1c92e4852637137f44938f8ae0426835
WaterBird <lcc1105897090@126.com>
2024-06-11 17:06:24 +0800

    解决bug，暂时取消了jwt校验

--- src/main/java/com/waterbird/wbapiinterface/controller/NameController.java
+++ src/main/java/com/waterbird/wbapiinterface/controller/NameController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/name")
 public class NameController {
+    @GetMapping("/get")
+    public String getUserNameByGet(){
+        return "你的名字是";
+    }
     @PostMapping("/user")
     public String getUserNameByPost(@RequestBody User user) {
         return "你的名字是"+user.getUserName();
--- src/test/java/com/waterbird/wbapiinterface/WbapiInterfaceApplicationTests.java
+++ src/test/java/com/waterbird/wbapiinterface/WbapiInterfaceApplicationTests.java
@@ -1,32 +1,13 @@
 package com.waterbird.wbapiinterface;
 
-import com.waterbird.wbapisdk.client.WbApiClient;
-import com.waterbird.wbapisdk.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class WbapiInterfaceApplicationTests {
 
-    // 注入一个名为yuApiClient的Bean
-    @Resource
-    private WbApiClient yuApiClient;
-    // 表示这是一个测试方法
     @Test
     void contextLoads() {
-        // 调用yuApiClient的getNameByGet方法，并传入参数"yupi"，将返回的结果赋值给result变量
-        String result = yuApiClient.getNameByGet("lcc");
-        // 创建一个User对象
-        User user = new User();
-        // 设置User对象的username属性为"liyupi"
-        user.setUserName("lcc");
-        // 调用yuApiClient的getUserNameByPost方法，并传入user对象作为参数，将返回的结果赋值给usernameByPost变量
-        String usernameByPost = yuApiClient.getUserNameByPost(user);
-        // 打印result变量的值
-        System.out.println(result);
-        // 打印usernameByPost变量的值
-        System.out.println(usernameByPost);
+
     }
 }
